9c3c207117dc57ee14148b43316fb81221fbd730
Domenic Cassisi <domenic.cassisi@web.de>
2018-11-22 13:02:06 +0100

    Added JavaDoc Plugin

--- src/main/java/de/cassisi/App.java
+++ src/main/java/de/cassisi/App.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
  */
 public class App 
 {
+    /**
+     * This is the main
+     * @param args the submitted arguments
+     */
     public static void main( String[] args )
     {
 	Scanner scanner = new Scanner(System.in);
